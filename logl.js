6e75975b7fecc2eef3d37b7fb805b5f77e507ee9
Dhiraj Kumar <56203115+Dhirajkumar908@users.noreply.github.com>
2021-04-01 14:49:17 +0530

    Add files via upload

---
+++ logl.js
@@ -0,0 +1,24 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<
+script src = "https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js" > < /script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+https: //firebase.google.com/docs/web/setup#available-libraries -->
+    <
+    script src = "https://www.gstatic.com/firebasejs/8.3.1/firebase-analytics.js" > < /script>
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBAC1N7k6OeTKH-kZh1-Rcc23JJvH8Z8Bg",
+    authDomain: "first-web-project-6de22.firebaseapp.com",
+    projectId: "first-web-project-6de22",
+    storageBucket: "first-web-project-6de22.appspot.com",
+    messagingSenderId: "182712436814",
+    appId: "1:182712436814:web:5471fa664afa5841da9b1f",
+    measurementId: "G-1NKPC1VM4Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
